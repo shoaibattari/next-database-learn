15461b77f1c9df9380c24b0b52a99487f6e7abc1
shoaib memon <shoaibattari83@gmail.com>
2023-05-03 23:45:20 +0500

    database check

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -5,9 +5,10 @@ export async function GET(request: NextRequest) {
     const client = await db.connect();
  
   try {
-    await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    const names = ['Fiona', 'Lucy'];
-    await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+    // await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+    const names = ['Fiona1', 'Lucy2'];
+    const owners = ['owners1', 'owners2'];
+    await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${owners[0]});`;
   } catch (error) {
     return NextResponse.json({ error }, {
         status: 500,
